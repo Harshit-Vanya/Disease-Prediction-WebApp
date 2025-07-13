b70fc46d2326f71a282c25c9f7f0bcbe5d28add6
Harshit Vanya <harshitvanyagmer@gmail.com>
2025-07-13 20:42:45 +0530

    Delete server directory

--- server/server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-import util
-from util import app
-
-if __name__ == '__main__':
-    util.load_artifacts()
-    app.run(host='0.0.0.0', port=5000, debug=True)
